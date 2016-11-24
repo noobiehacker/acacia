d975232848fcd5b32d79a6e5b0ae710e6a0cde7a
noobiehacker <davidkwokhochan@gmail.com>
2016-11-23 17:04:23 -0800

    Parsed Download Link

--- function.py
+++ function.py
@@ -1,17 +1,37 @@
 import unittest
+import requests
+from bs4 import BeautifulSoup
 
 #Enter a Search Input
 
 def scanInput():
-    person = raw_input('Enter Your Search Input: ')
-    print("Hello", person)
+    #person = raw_input('Enter Your Search Input: ')
+    #print("Hello", person)
     return True
 
 #Query youtube with search input and return result as HTML String
+def queryYoutube(input):
+    prefix = "https://www.youtube.com/results?search_query="
+    url = prefix + input
+    page = requests.get(url)
+    return page.content
 
+def isWatchLink(link):
+    return link.startswith("/watch?v=")
 
 #Given an HTML Youtube Result page, return first X results parsed into a String Array
-
+def parseDownloadLink(page):
+    soup = BeautifulSoup(page, 'html.parser')
+    array = set()
+    for link in soup.find_all('a'):
+        linkhref = link.get('href')
+        if isWatchLink(linkhref):
+            prefix = "https://www.youtube.com/results?search_query="
+            youtubeLink = prefix + linkhref
+            array.add(youtubeLink)
+    for link in array:
+        print(link)
+    return array
 
 #Given a Youtube link, download the result
 
@@ -22,7 +42,24 @@ def scanInput():
 class functionTests(unittest.TestCase):
 
     def testScanInput(self):
-        self.failUnless(scanInput())
+        self.assertTrue(scanInput())
+
+    def testQueryYoutube(self):
+        input= "Stuff"
+        actural = queryYoutube(input)
+        self.assertTrue(len(actural) > 500)
+
+    def testParseDownloadLink(self):
+        page = queryYoutube("stuff")
+        result = parseDownloadLink(page)
+        self.assertIsNotNone(result)
+        self.assertTrue(len(result) > 9)
+
+    def testIsWatchLink(self):
+        success = "/watch?v=7RDSLzHJ74I"
+        self.assertTrue(isWatchLink(success))
+        failure = "ABCDE"
+        self.assertFalse(isWatchLink(failure))
 
 def main():
     unittest.main()

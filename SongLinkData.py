63d8491fd88a3a5c173e09141e8d2fe7c2d326ef
David Chan <davidkwokhochan@gmail.com>
2017-02-24 16:29:27 -0800

    Added Song Link Data Structure

---
+++ SongLinkData.py
@@ -0,0 +1,9 @@
+import unittest
+# Here's our "unit tests".
+class functionTests(unittest.TestCase):
+
+    def testOne(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
--- features/steps/parseJsonSteps.py
+++ features/steps/parseJsonSteps.py
@@ -1,7 +1,12 @@
+import logging
 from behave import given, when, then, step
 
 @given('Function is called')
 def step_impl(context):
+    import logging
+    loglevel = logging.DEBUG
+    logging.basicConfig(level=loglevel)
+    logging.debug("Some messages to be shown just when debugging or unittesting")
     pass
 
 @when('Json is loaded{number:d}')
